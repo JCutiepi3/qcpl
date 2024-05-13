f69ccc5642640e11618483bea0414586cf6fd1e3
ciskingb <ciskingb.personal@gmail.com>
2024-05-13 11:26:32 +0800

    Update authentication.php

--- Backend/authentication.php
+++ Backend/authentication.php
@@ -114,9 +114,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div>
                 <input type="submit" value="Login">
-            </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>
     </div>    
 </body>
 </html>
