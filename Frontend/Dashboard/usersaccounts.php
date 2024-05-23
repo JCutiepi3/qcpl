cef14379997610fb423e78242054b1b2fe6e3177
Guillermo Mercado <mercado.g.bscs@gmail.com>
2024-05-23 10:25:16 +0800

    ok

--- Frontend/Dashboard/usersaccounts.php
+++ Frontend/Dashboard/usersaccounts.php
@@ -10,24 +10,24 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql_admins = "SELECT * FROM admins";
+$sql_admins = "SELECT * FROM users";
 $result_admins = $conn->query($sql_admins);
 
 if ($result_admins->num_rows > 0) {
-    echo "<h2>ADMINS</h2>";
-    echo "<table aria-describedby='admin-table'>";
+    echo "<h2>USERS</h2>";
+    echo "<table aria-describedby='user-table'>";
     echo "<tr><th>ID</th><th>Name</th><th>Division</th><th>Username</th><th>Password</th><th colspan='2'>Action</th></tr>";
     echo "</thead>";
     echo "<tbody>";
     while ($row = $result_admins->fetch_assoc()) {
         echo "<tr>";
-        echo "<td>" . "<center>" . htmlspecialchars($row["id"]) . "</td>";
-        echo "<td>" . htmlspecialchars($row["name"]) . "</td>";
-        echo "<td>" . htmlspecialchars($row["division"]) . "</td>";
-        echo "<td>" . htmlspecialchars($row["username"]) . "</td>";
-        echo "<td>" . str_repeat("*", strlen($row["password"])) . "</td>";
-        echo "<td><a href='updateuseradmin.php?id=" . htmlspecialchars($row["id"]) . "'>Edit</a></td>";
-        echo "<td><a href='deleteuseradmin.php?id=" . htmlspecialchars($row["id"]) . "'>Delete</a></td>";
+        echo "<td><center>" .  htmlspecialchars($row["id"]) . "</td>";
+        echo "<td><center>" . htmlspecialchars($row["name"]) . "</td>";
+        echo "<td><center>" . htmlspecialchars($row["division"]) . "</td>";
+        echo "<td><center>" . htmlspecialchars($row["username"]) . "</td>";
+        echo "<td><center>" . str_repeat("*", strlen($row["password"])) . "</td>";
+        echo "<td><center><a href='updateuseradmin.php?id=" . htmlspecialchars($row["id"]) . "'>Edit</a></td>";
+        echo "<td><center><a href='deleteuseradmin.php?id=" . htmlspecialchars($row["id"]) . "'>Delete</a></td>";
         echo "</tr>";
     }
     echo "</tbody>";
