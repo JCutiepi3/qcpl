106200addbc16dd27d7bb8ffc70b3e8bb4ef9413
Jeysee <148066540+JCutiepi3@users.noreply.github.com>
2024-06-05 13:08:34 +0800

    ok

--- Frontend/userdashboard.php
+++ Frontend/userdashboard.php
@@ -122,29 +122,6 @@
     <!-- =========== Scripts =========  -->
     <script src="main.js"></script>
 
-    <script>
-let list = document.querySelectorAll(".navigation li");
-
-function activeLink() {
-  list.forEach((item) => {
-    item.classList.focus("hovered")
-
-  });
-  this.classList.add("hovered");
-}
-
-list.forEach((item) => item.addEventListener("mouseover", activeLink));
-
-// Menu Toggle
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
-
-toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
-};</script>
-
     <!-- ====== ionicons ======= -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
